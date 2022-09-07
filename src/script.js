389920464ffc18c147fb8b1d7b382144d7fb6797
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-09-07 15:08:35 +0530

    Final Commit

---
+++ src/script.js
@@ -0,0 +1,11 @@
+var num;
+num = 0;
+
+function decreament(){
+    num = num - 1;
+    document.getElementById("b2").innerHTML=num;
+}
+function increament(){
+    num = num + 1;
+    document.getElementById('b2').innerHTML=num;
+}
